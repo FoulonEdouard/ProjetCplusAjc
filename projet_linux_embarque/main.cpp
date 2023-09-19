d90fd7caf27fc1eaa546180fc0bbd538161b3316
FoulonEdouard <140059867+FoulonEdouard@users.noreply.github.com>
2023-09-19 12:27:33 +0200

    Add files via upload

--- projet_linux_embarque/main.cpp
+++ projet_linux_embarque/main.cpp
@@ -163,18 +163,28 @@ int main(int argc, char** argv) {
       // 2EME IMAGE:  HISTOGRAMME
                gdImagePtr im2;
                FILE *pngout2;
-               im2 = gdImageCreate(1400,600);
-               int black2,white2,orange2;
+               im2 = gdImageCreate(1500,730);
+               int grisfonce2;
+               grisfonce2=gdImageColorAllocate(im, 230, 230, 230);
+               int black2,white2,orange2,blue2,marron2,red2,red3,orange3,blue3,marron3;
                white2 = gdImageColorAllocate(im2, 255, 255, 255);
                black2 = gdImageColorAllocate(im2, 0, 0, 0);
                orange2= gdImageColorAllocate(im2, 255, 128, 0);
-               char titre2[]={"difference % residents temperature reelle et normale par semaine"};
+               blue2 = gdImageColorAllocate(im2, 0, 0, 255);
+               marron2=gdImageColorAllocate(im2, 88, 41, 0);
+               red2 = gdImageColorAllocate(im, 255, 0, 0);
+               orange3= gdImageColorAllocate(im2, 205, 88, 0);
+               blue3 = gdImageColorAllocate(im2, 0, 0, 150);
+               marron3=gdImageColorAllocate(im2, 44, 20, 0);
+               red3 = gdImageColorAllocate(im, 150, 0, 0);
+
+               char titre2[]={"Differences % consommations pour les residents temperature reelle et normale par semaine"};
 
                gdImageString(im2, fontptr,
-                           50,
+                           400,
                            30,
                            (unsigned char*)titre2, foreground1);
-                   gdImageLine(im2,50,50,565,50,black2);
+                   gdImageLine(im2,390,50,1120,50,black2);
 
 
 
@@ -189,38 +199,101 @@ int main(int argc, char** argv) {
                           float a1=data[i]["fields"]["conso_a_tr"];
                           float b1=data[i]["fields"]["conso_a_tn"];
                           float c1=(a1/b1)*100-100;
+                          string c2string=to_string(floor(10*c1)/10);
+                          if(!c2string.empty()){c2string.resize(c2string.size()-5);}
 
-                            if(c1<0){c1=c1*(-1);}
-                              double c2=floor(10*c1)/10;
-                              string c2string=to_string(c2);
-                              gdImageRectangle(im2,40+(i*30),400-(c1*10),70+(i*30),400,orange2);
 
-                              gdImageString(im2, fontptr,
-                                             50+(i*30),
+
+                              if(c1>=0)
+                              {gdImageString(im2, fontptr,
+                                             40+(i*30),
                                              380-(c1*10),
-                                          (unsigned char*)c2string.c_str(), foreground1);
+                                          (unsigned char*)c2string.c_str(), foreground1);}
+                              if(c1<=0)
+                              {gdImageString(im2, fontptr,
+                                    32+(i*30),
+                                    410-(c1*10),
+                                    (unsigned char*)c2string.c_str(), foreground1);}
 
-                              if(i==0 or i==4 or i==8 or i==12 or i==16 or i==20 or i==24 or i==28 or i==32 or i==36 or i==40 or i==43)
+
+                              if(i<=3 or i>=41)
+                               {  gdImageFilledRectangle(im2,40+(i*30),400-(c1*10),70+(i*30),400,red2);
+                                  gdImageRectangle(im2,40+(i*30),400-(c1*10),70+(i*30),400,red3);}
+                              if(i>=4 && i<=16)
+                               {  gdImageFilledRectangle(im2,40+(i*30),400-(c1*10),70+(i*30),400,marron2);
+                                  gdImageRectangle(im2,40+(i*30),400-(c1*10),70+(i*30),400,marron3);}
+                              if(i>=17 && i<=27)
+                               {  gdImageFilledRectangle(im2,40+(i*30),400-(c1*10),70+(i*30),400,blue2);
+                                  gdImageRectangle(im2,40+(i*30),400-(c1*10),70+(i*30),400,blue3);}
+                              if(i>=28 && i<=40)
+                               {  gdImageFilledRectangle(im2,40+(i*30),400-(c1*10),70+(i*30),400,orange2);
+                                  gdImageRectangle(im2,40+(i*30),400-(c1*10),70+(i*30),400,orange3);}
+
+
+
+
+                              if(i==0 or i==4 or i==8 or i==12 or i==16 or i==24 or i==28 or i==39 or i==43)
                               {gdImageString(im2, fontptr,
                                       20+(i*30),
-                                      430,
+                                      700,
                                       (unsigned char*)datejson.c_str(), foreground1);
-                              gdImageLine(im2,55+(i*30),400,55+(i*30),420,black2);}
+                                  for(int i3=0;i3<=14;i3++)
+                                  {gdImageLine(im2,55+(i*30),400+(i3*20),55+(i*30),410+(i3*20),black2);}}
 
-                       cout<<datejson<<" à la conso diff    "<<c2<<endl;}
+                              if(i==20 or i==31 or i==36)
+                              {gdImageString(im2, fontptr,
+                                      20+(i*30),
+                                      100,
+                                      (unsigned char*)datejson.c_str(), foreground1);
+                                  for(int i3=0;i3<=13;i3++)
+                                  {gdImageLine(im2,55+(i*30),390-(i3*20),55+(i*30),400-(i3*20),black2);}}
+
+                       cout<<datejson<<" à la conso diff    "<<c2string<<endl;}
                      }
                 }}
 
                // Axe ordonnée
-               gdImageLine(im2,40,100,40,400,black2);
+               gdImageLine(im2,40,100,40,680,black2);
                gdImageLine(im2,40,100,30,110,black2);
                gdImageLine(im2,40,100,50,110,black2);
+               gdImageLine(im2,40,680,30,670,black2);
+               gdImageLine(im2,40,680,50,670,black2);
+               char axeordo[50]="% Difference consomation en TWh enntre tr et tn";
+               gdImageString(im2, fontptr,
+                              60,
+                              100,
+                           (unsigned char*)axeordo, foreground1);
 
                // Axe abcisse
-               gdImageLine(im2,40,400,1370,400,black2);
-               gdImageLine(im2,1370,400,1360,410,black2);
-               gdImageLine(im2,1370,400,1360,390,black2);
+               gdImageLine(im2,40,400,1410,400,black2);
+               gdImageLine(im2,1410,400,1400,410,black2);
+               gdImageLine(im2,1410,400,1400,390,black2);
+               char axeabs[50]="Date debut_semaine";
+               gdImageString(im2, fontptr,
+                              1350,
+                              420,
+                           (unsigned char*)axeabs, foreground1);
 
+               //Légende
+               char legende[9]="Legende:";
+               char ete[4]="Ete";
+               char automne[8]="Automne";
+               char hiver[6]="Hiver";
+               char printemps[10]="Printemps";
+                gdImageFilledRectangle(im2,1300,50,1350,70,red2);
+                gdImageFilledRectangle(im2,1300,80,1350,100,marron2);
+                gdImageFilledRectangle(im2,1300,110,1350,130,blue2);
+                gdImageFilledRectangle(im2,1300,140,1350,160,orange2);
+                gdImageRectangle(im2,1300,50,1350,70,red3);
+                gdImageRectangle(im2,1300,80,1350,100,marron3);
+                gdImageRectangle(im2,1300,110,1350,130,blue3);
+                gdImageRectangle(im2,1300,140,1350,160,orange3);
+                gdImageString(im2, fontptr, 1300, 25, (unsigned char*)legende,foreground1);
+                gdImageLine(im2,1300,40,1360,40,black2);
+                gdImageString(im2, fontptr, 1360, 55, (unsigned char*)ete,foreground1);
+                gdImageString(im2, fontptr, 1360, 85, (unsigned char*)automne,foreground1);
+                gdImageString(im2, fontptr, 1360, 115, (unsigned char*)hiver,foreground1);
+                gdImageString(im2, fontptr, 1360, 145, (unsigned char*)printemps,foreground1);
 
                pngout2 = fopen("/home/edouard/projet_linux_embarque/testprojethisto.png", "wb");
                gdImagePng(im2, pngout2);
